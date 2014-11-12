5a60be61c5a972c5c1450a1e4264f5d4f071c5dc
Pete Peterson <petersonpf@ornl.gov>
2014-11-12 15:18:22 -0500

    Still messing with favicons

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    # url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
     url(r'^admin/', include(admin.site.urls)),
 )
