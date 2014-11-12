a8118a6996e1e5cd52a59466b19cdb03269c689a
Pete Peterson <petersonpf@ornl.gov>
2014-11-12 16:39:43 -0500

    Another thing to help with static urls

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -18,4 +21,4 @@ urlpatterns = patterns('',
     url(r'^admin/$', include(admin.site.urls)),
     url(r'^api/$',include(router.urls)),
     url(r'^api-auth/$',include('rest_framework.urls', namespace='rest_framework')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
