7508ae68a27f7b5cb947e37de47357f75cbab19c
Pete Peterson <petersonpf@ornl.gov>
2014-11-13 10:50:37 -0500

    Trying to get the favicon showing

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    # url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
+    url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/',include(router.urls)),
     url(r'^api-auth/',include('rest_framework.urls', namespace='rest_framework')),
