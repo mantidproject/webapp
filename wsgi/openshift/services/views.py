2f9849948dd8e76dd7d787d2c25912033ef24976
Pete Peterson <petersonpf@ornl.gov>
2014-11-13 15:23:45 -0500

    Changing with message permissions

--- wsgi/openshift/services/views.py
+++ wsgi/openshift/services/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Message
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializer import MessageSerializer
 
-class MessageViewSet(viewsets.ReadOnlyModelViewSet):
+class MessageViewSet(viewsets.ModelViewSet):
   queryset = Message.objects.all()
   serializer_class = MessageSerializer
+  permission_classes = [IsAuthenticatedOrReadOnly]
