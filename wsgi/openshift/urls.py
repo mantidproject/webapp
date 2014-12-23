ab4cea887704a52087f61e72dfd1e17ffe5dc75d
Pete Peterson <petersonpf@ornl.gov>
2014-12-23 10:10:12 -0500

    Hiding the messaging endpoint from the site.

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -11,7 +11,7 @@ from rest_framework import routers
 from services.views import MessageViewSet, UsageViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'message', MessageViewSet)
+#router.register(r'message', MessageViewSet)
 router.register(r'usage', UsageViewSet)
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
