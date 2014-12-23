cd44d3e9644316b07eea726be5aea90a88012a2a
Pete Peterson <petersonpf@ornl.gov>
2014-12-23 16:05:09 -0500

    Added small amount of api docs

--- wsgi/openshift/services/models.py
+++ wsgi/openshift/services/models.py
@@ -10,15 +10,15 @@ class Message(models.Model):
       return self.author
 
 class Usage(models.Model):
-    uid = models.CharField(max_length=32)           # md5 ex: "c5a9b601408709f47417bcba3571262b"
-    host = models.CharField(max_length=32)          # md5 ex: "7defb184ceadab4e79eff323359ad373"
+    uid = models.CharField(max_length=32, help_text="md5 version of username")           # md5 ex: "c5a9b601408709f47417bcba3571262b"
+    host = models.CharField(max_length=32, help_text="md5 version of hostname")          # md5 ex: "7defb184ceadab4e79eff323359ad373"
     dateTime = models.DateTimeField()               # ex: "2014-12-08T18:50:35.817942000"
     osName = models.CharField(max_length=32)        # ex: "Linux"
     osArch = models.CharField(max_length=16)        # ex: "x86_64"
     osVersion = models.CharField(max_length=32)     # ex: "3.17.4-200.fc20.x86_64"
     ParaView = models.CharField(max_length=16)      # ex: "3.98.1"
     mantidVersion = models.CharField(max_length=32) # ex: "3.2.20141208.1820"
-    mantidSha1 = models.CharField(max_length=40)    # sha1 ex: "e9423bdb34b07213a69caa90913e40307c17c6cc"
+    mantidSha1 = models.CharField(max_length=40, help_text="sha1 for specific mantid version")    # sha1 ex: "e9423bdb34b07213a69caa90913e40307c17c6cc"
     osReadable = models.CharField(max_length=80, default="", blank=True) # ex: "Fedora 20 (Heisenbug)"
     application = models.CharField(max_length=80, default="", blank=True)
     component = models.CharField(max_length=80, default="", blank=True)
