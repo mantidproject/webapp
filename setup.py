36ec3c1e7dfd4e6adec39d095ada2ffb3a13511d
Pete Peterson <petersonpf@ornl.gov>
2014-12-31 10:16:41 -0500

    Adding django-filter to the system.

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import os
 packages = ['Django<=1.6',
     'static3', # serve static files in the server
     'djangorestframework==2.4.4',
+    'django-filter',
     'South']
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
