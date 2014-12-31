693dc912c5ecef7d9b7cc17911fcdb1c95dfe708
Pete Peterson <petersonpf@ornl.gov>
2014-12-31 11:17:00 -0500

    GFinally got date filtering working.

--- wsgi/openshift/services/views.py
+++ wsgi/openshift/services/views.py
@@ -5,18 +5,40 @@ from .models import Message, Usage
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from .serializer import MessageSerializer, UsageSerializer
+import django_filters
 
 class MessageViewSet(viewsets.ModelViewSet):
   queryset = Message.objects.all()
   serializer_class = MessageSerializer
   permission_classes = [IsAuthenticatedOrReadOnly]
 
+class WithinDateFilter(django_filters.DateFilter):
+    def filter(self, queryset, value):
+        from datetime import timedelta
+        if value:
+            # date_value = value.replace(hour=0, minute=0, second=0)
+            filter_lookups = {
+                "%s__range" % (self.name, ): (
+                    value,
+                    value + timedelta(days=1),
+                ),
+            }
+            queryset = queryset.filter(**filter_lookups)
+        return queryset
+
+class UsageFilter(django_filters.FilterSet):
+    date    = WithinDateFilter(name="dateTime")
+    datemin = django_filters.DateFilter(name="dateTime", lookup_type='gte')
+    datemax = django_filters.DateFilter(name="dateTime", lookup_type='lt')
+
+    class Meta:
+        model = Usage
+        fields = ['date', 'datemin','datemax', 'host', 'uid']
+        order_by = ['-dateTime']
+
 class UsageViewSet(viewsets.ModelViewSet):
   queryset = Usage.objects.all()
   serializer_class = UsageSerializer
   permission_classes = [AllowAny]
-  filter_fields = ('uid', 'host')
-
-  # by default show newest first
-  filter_backends = (filters.OrderingFilter,)
-  ordering=('-dateTime')
+  filter_class=UsageFilter
+  filter_backends = (filters.OrderingFilter, filters.DjangoFilterBackend,)
