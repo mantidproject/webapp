70c638f56a16d70ba3d16ef7fcc20b74b01858c0
Pete Peterson <petersonpf@ornl.gov>
2015-01-14 10:40:13 -0500

    Using Django 1.7's version of field validation.

--- wsgi/openshift/services/serializer.py
+++ wsgi/openshift/services/serializer.py
@@ -16,36 +16,32 @@ class UsageSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Usage
 
-  def checkLength(self, attrs, source, length, label):
-    value = attrs[source]
+  def checkLength(self, value, length, label):
     if (len(value) != length):
       raise serializers.ValidationError("value is not consistent with %s (length %d != %d)" % (label, len(value), length))
-    return attrs
 
-  def checkHex(self, attrs, source, label):
-    value = attrs[source]
+  def checkHex(self, value, label):
     for i in "0123456789abcdef":
       value = value.replace(i, "")
 
     if len(value) > 0:
       raise serializers.ValidationError("'%s' is not consistent with %s" % (attrs[source], label))
-    return attrs
 
-  def validate_uid(self, attrs, source):
+  def validate_uid(self, value):
     """uid should be an md5"""
-    attrs = self.checkLength(attrs, source, 32, "md5")
-    return attrs
+    self.checkLength(value, 32, "md5")
+    return value
 
-  def validate_host(self, attrs, source):
+  def validate_host(self, value):
     """host should be an md5"""
-    attrs = self.checkLength(attrs, source, 32, "md5")
-    return attrs
+    self.checkLength(value, 32, "md5")
+    return value
 
-  def validate_mantidSha1(self, attrs, source):
+  def validate_mantidSha1(self, value):
     """mantidSha1 should be an sha1"""
-    attrs = self.checkLength(attrs, source, 40, "sha1")
-    attrs = self.checkHex(attrs, source, "sha1")
-    return attrs
+    self.checkLength(value, 40, "sha1")
+    self.checkHex(value, "sha1")
+    return value
 
 class HostListSerializer(serializers.Serializer):
     host = serializers.CharField()
