75dc4f0daff5f8f2619dac90f428c4d23e8c3e25
Pete Peterson <petersonpf@ornl.gov>
2015-01-21 09:14:29 -0500

    Changing the reports page to be the main one.

--- wsgi/openshift/report/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from report import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^host/(?P<md5>\w+)?$', views.host, name='host'),
-    url(r'^user/(?P<md5>\w+)?$', views.user, name='user'),
-)
--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -8,14 +8,17 @@ admin.autodiscover()
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+import report
+
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/favicon.ico'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('services.urls')),
     url(r'^api-auth/',include('rest_framework.urls', namespace='rest_framework')), # should be in services/urls.py
-    url(r'^report/', include('report.urls')),
+    url(r'^$', report.views.index, name='index'),
+    url(r'^host/(?P<md5>\w+)?$', report.views.host, name='host'),
+    url(r'^user/(?P<md5>\w+)?$', report.views.user, name='user'),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + staticfiles_urlpatterns()
