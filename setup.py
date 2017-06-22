1b7db5fbf0abf8889027f4a780312014b6f6500c
Micah Raney <micah-raney@users.noreply.github.com>
2017-06-22 11:41:52 -0400

    Another auto-format

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import os
 
 # Put here required packages
 packages = ['Django==1.11.2',
-    'static3', # serve static files in the server
-    'djangorestframework==3.6.3',
-    'django-filter==1.0.4',
-    'Markdown',
-    'MySQL-python']
+            'static3',  # serve static files in the server
+            'djangorestframework==3.6.3',
+            'django-filter==1.0.4',
+            'Markdown',
+            'MySQL-python']
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
-     packages.append('django-redis-cache')
-     packages.append('hiredis')
+    packages.append('django-redis-cache')
+    packages.append('hiredis')
 
 setup(name='MantidReports',
       version='1.0',
@@ -23,4 +23,4 @@ setup(name='MantidReports',
       install_requires=packages,
       packages=find_packages(),
       include_package_data=True,
-)
+      )
