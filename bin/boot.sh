2d2aa477ad60fcc4daf82459c813c797560ac089
Martyn Gigg <martyn.gigg@gmail.com>
2017-12-19 11:29:25 +0000

    Minor cleanup of public ports and environment variables
    Django's SECRET_KEY is now create automatically on boot. DEBUG environment
    variable

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 # Starts the stack
-set -ex
+set -e
+
+function mk_django_secret() {
+  python -c "import random,string;print 'SECRET_KEY=\"%s\"'%''.join([random.SystemRandom().choice(\"{}{}{}\".format(string.ascii_letters, string.digits, string.punctuation)) for i in range(63)])";
+}
 
 SCRIPTPATH=$(cd "$(dirname "$0")"; pwd -P)
 SOURCE_DIR=$(cd "$SCRIPTPATH" && cd .. && pwd -P)
 
 COMPOSE_FILES="--file ${SOURCE_DIR}/docker-compose.yml --file ${SOURCE_DIR}/docker-compose-db-import.yml"
 PROJECT_NAME=reports
+
+# Required by django settings
+DB_SERVICE=postgres
+DB_PORT=5432
+# Create a secret key
+SECRET_KEY=$(mk_django_secret)
+export DB_SERVICE DB_PORT SECRET_KEY
 
 if [ $# -eq 1 ]; then
   if [ ! -f "$1" ]; then
@@ -19,13 +30,13 @@ if [ $# -eq 1 ]; then
   fi
   export DB_DUMP
 else
-  echo "Usage: $0 path-to-sql-dump.sql"
+  echo "Usage: $0 path-to-postgres-sql-dump.sql"
   exit 1
 fi
 
 if [ ! -f ${SOURCE_DIR}/.env ]; then
   echo "Unable to find environment file ${SOURCE_DIR}/.env. It must contain the following variables:"
-  echo "  DB_NAME, DB_USER, DB_PASS, SECRET_KEY, DB_SERVICE, DB_PORT"
+  echo "  DB_NAME, DB_USER, DB_PASS"
   exit 1
 fi
 
