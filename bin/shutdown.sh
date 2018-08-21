002a9540f5a432d0400100b76a0807ce6a8f211f
Martyn Gigg <martyn.gigg@gmail.com>
2018-08-21 13:12:07 +0100

    Mark bin/shutdown as executable.

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -8,4 +8,6 @@ PROJECT_NAME=errorreports
 
 cd ${SOURCE_DIR}
 docker-compose down
+# The webdata should not really be a volume as all of the files come out of the image
+# We remove it so the next rebuild sees the most recent version of the Python code
 docker volume rm ${PROJECT_NAME}_webdata
