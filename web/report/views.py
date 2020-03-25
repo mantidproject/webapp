582e651991f0de9db255bf73ea327537ca6054a6
Mantid-Matthew <matthew.andrew@stfc.ac.uk>
2020-03-25 13:50:26 +0000

    Shifted reports to Django 2

--- web/report/views.py
+++ web/report/views.py
@@ -8,18 +8,16 @@ from django.template import RequestContext, loader
 def index(request):
     return render(request, 'index.html')
 
+def host_list(request):
+    return render(request, 'host_list.html')
 
-def host(request, md5):
-    if md5 is None:
-        return render(request, 'host_list.html')
-    else:
-        context = {'host': md5}
-        return render(request, 'host.html', context)
+def host(request, hostid):
+    context = {'host': hostid}
+    return render(request, 'host.html', context)
 
+def user_list(request):
+    return render(request, 'user_list.html')
 
-def user(request, md5):
-    if md5 is None:
-        return render(request, 'user_list.html')
-    else:
-        context = {'uid': md5}
-        return render(request, 'user.html', context)
+def user(request, userid):
+    context = {'uid': userid}
+    return render(request, 'user.html', context)
